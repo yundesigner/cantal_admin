ad914476255aaf7aa79f5ec27b8de4ecb0e38985
yun <you@example.com>
2021-10-01 14:03:15 +0900

    eventpage modal

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-const modal = document.getElementById("modal");
-const btnModal = document.getElementById("btn-modal");
-btnModal.addEventListener("click", (e) => {
-  modal.style.display = "flex";
-});
-const closeBtn = modal.querySelector(".modal_close");
-closeBtn.addEventListener("click", (e) => {
-  modal.style.display = "none";
-});
+// Modal창 열기
+function openModal(num) {
+  $(".modal" + num).css("display", "flex");
+
+  $(".modal_close").click(function () {
+    $(".modal" + num).css("display", "none");
+  });
+}
