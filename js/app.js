03c5bc6af0050d255e1c94acec00d02391c01f52
yun <you@example.com>
2021-10-06 10:01:00 +0900

    1006

--- js/app.js
+++ js/app.js
@@ -6,3 +6,13 @@ function openModal(num) {
     $(".modal" + num).css("display", "none");
   });
 }
+
+// 댓글 카운터
+
+const comment = document.querySelector("tbody");
+const eleCount = comment.childElementCount;
+
+window.onload = function countComment() {
+  const count = document.querySelector(".comment_counter");
+  count.innerHTML = "총 " + eleCount + "개";
+};
