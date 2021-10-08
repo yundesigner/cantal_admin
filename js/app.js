578e523fd90abbaee5ae1a6d1a8395a93abbfe51
famousjedo <thebestfancy@naver.com>
2021-10-08 15:38:10 +0900

    211008_매칭관리페이지완료

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+// 로그인 창
+function login(){
+  const login = document.querySelector('.login');
+  const loginId = document.getElementsByName('loginId')[0].value;
+  const loginPw = document.getElementsByName('loginPw')[0].value;
+
+  if(loginId.length > 3 && loginPw.length > 7){
+    login.style.display = 'none'
+  }
+}
+
 // Modal창 열기
 function openModal(num) {
   $(".modal" + num).css("display", "flex");
@@ -7,12 +18,21 @@ function openModal(num) {
   });
 }
 
-// 댓글 카운터
+// 매칭관리 - 상담내용
+// 토글
+const counselToggle = document.querySelectorAll('.btn_detail');
 
-const comment = document.querySelector("tbody");
-const eleCount = comment.childElementCount;
+var toggleTrue = true;
 
-window.onload = function countComment() {
-  const count = document.querySelector(".comment_counter");
-  count.innerHTML = "총 " + eleCount + "개";
-};
+for(var i=0; i < counselToggle.length; i++){
+  counselToggle[i].addEventListener('click', function(){ 
+    const parentTr = $(this).parents('tr')
+  
+    if(toggleTrue){
+      $('<tr class="counsel_look"><td colspan="7"><textarea></textarea><div class="btn_wrap"><button class="block cancel">삭제</button><button type="submit" class="match_status_submit">수정</button></div></td></tr>').insertAfter(parentTr);
+    }else{
+      parentTr.next().css('display', 'none')
+    }
+    toggleTrue = !toggleTrue
+  })
+}
